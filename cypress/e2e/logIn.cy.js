a8ab6e6a602e7566cfa96209977fb9def7b563e9
Aung Myat Thu <bororo.vox@gmail.com>
2024-12-21 17:37:15 +0630

    Add more users in log in.

--- cypress/e2e/logIn.cy.js
+++ cypress/e2e/logIn.cy.js
@@ -1,7 +1,27 @@
-const { givenLogInAsStandardUser } = require('../support/logIn');
+const {
+    givenLogInAsStandardUser,
+    givenLogInAsLockedOutUser,
+    givenLogInAsProblemUser,
+    givenLogInAsPerformanceGlitchUser
 
-describe('template spec', () => {
-  it('passes', () => {
-    givenLogInAsStandardUser();
-  });
-});
+} = require('../support/logIn')
+
+describe('Log In', () => {
+
+    it('logs in as standard user', () => {
+        givenLogInAsStandardUser()
+    })
+
+    it('logs in as locked out user', () => {
+        givenLogInAsLockedOutUser()
+    })
+
+    it('logs in as problem user', () => {
+        givenLogInAsProblemUser()
+    })
+
+    it('logs in as performance glitch user', () => {
+        givenLogInAsPerformanceGlitchUser()
+    })
+
+})
--- cypress/support/logIn.js
+++ cypress/support/logIn.js
@@ -1,22 +1,40 @@
-const credentials = require('../fixtures/credentials.json');
+const credentials = require('../fixtures/credentials.json')
+const baseURL = 'https://www.saucedemo.com/v1/index.html'
 
 const givenLogInAsStandardUser = () => {
-    cy.visit('https://www.saucedemo.com/v1/index.html');
-    cy.get('div.login-box').find('input[name="user-name"]').type(credentials[0].username);
-    cy.get('div.login-box').find('input[name="password"]').type(credentials[0].password);
-    cy.get('div.login-box').find('input[id="login-button"]').click();
-    cy.wait(3000);
-};
+    cy.visit(baseURL)
+    cy.get('div.login-box').find('input[name="user-name"]').type(credentials[0].username)
+    cy.get('div.login-box').find('input[name="password"]').type(credentials[0].password)
+    cy.get('div.login-box').find('input[id="login-button"]').click()
+    cy.wait(3000)
+}
 
 const givenLogInAsLockedOutUser = () => {
-    cy.visit('https://www.saucedemo.com/v1/index.html');
-    cy.get('div.login-box').find('input[name="user-name"]').type(credentials[1].username);
-    cy.get('div.login-box').find('input[name="password"]').type(credentials[0].password);
-    cy.get('div.login-box').find('input[id="login-button"]').click();
-    cy.wait(3000);
-};
+    cy.visit(baseURL)
+    cy.get('div.login-box').find('input[name="user-name"]').type(credentials[1].username)
+    cy.get('div.login-box').find('input[name="password"]').type(credentials[0].password)
+    cy.get('div.login-box').find('input[id="login-button"]').click()
+    cy.wait(3000)
+}
 
+const givenLogInAsProblemUser = () => {
+    cy.visit(baseURL)
+    cy.get('div.login-box').find('input[name="user-name"]').type(credentials[2].username)
+    cy.get('div.login-box').find('input[name="password"]').type(credentials[0].password)
+    cy.get('div.login-box').find('input[id="login-button"]').click()
+    cy.wait(3000)
+}
+
+const givenLogInAsPerformanceGlitchUser = () => {
+    cy.visit(baseURL)
+    cy.get('div.login-box').find('input[name="user-name"]').type(credentials[3].username)
+    cy.get('div.login-box').find('input[name="password"]').type(credentials[0].password)
+    cy.get('div.login-box').find('input[id="login-button"]').click()
+    cy.wait(3000)
+}
 module.exports = {
     givenLogInAsStandardUser,
-    givenLogInAsLockedOutUser
-};
+    givenLogInAsLockedOutUser,
+    givenLogInAsProblemUser,
+    givenLogInAsPerformanceGlitchUser
+}
