3b8d563837034085d67dc5d547b3a2a9f3f935d4
Aung Myat Thu <bororo.vox@gmail.com>
2024-12-30 20:09:12 +0630

    Set up for component test.

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,33 @@
-const {defineConfig} = require('cypress')
+const { defineConfig } = require("cypress");
 
 // Variable to store the order number, shared between tasks
-let total
+let total;
 
 // Export Cypress configuration using defineConfig
 module.exports = defineConfig({
-    e2e: {
-        // Setup function for registering Node event listeners
-        setupNodeEvents(on, config) {
-            on('task', {
-                setOrderTotal,
-                getOrderTotal
-            })
-        },
-        // Set the base URL for the application under test
-        baseUrl: 'https://www.saucedemo.com/v1/index.html'
-    }
-})
+  e2e: {
+    // Setup function for registering Node event listeners
+    setupNodeEvents(on, config) {
+      on("task", {
+        setOrderTotal,
+        getOrderTotal,
+      });
+    },
+    // Set the base URL for the application under test
+    baseUrl: "https://www.saucedemo.com/v1/index.html",
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
 
 const setOrderTotal = (totalPrice) => {
-    total = totalPrice
-    return total
-}
+  total = totalPrice;
+  return total;
+};
 
-const getOrderTotal = () => total
+const getOrderTotal = () => total;
